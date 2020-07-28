186785249adfb660ca7979d153a05c6eec1c2825
Damascan-Alex <doxvirtual@gmail.com>
2020-07-28 18:01:20 +0700

    Importance-Range-Slider

---
+++ src/components/rangeSlider.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class ImportanceRange extends React.Component {
+/*   constructor(props) {
+    super(props);
+    //this.updateRange = this.updateRange.bind(this);
+  } */
+
+  updateRange = (e) => {
+    // this balue can export
+    /* this.props.updateRange(e.target.value); */
+  }
+
+  render() {
+    // console.log(this.props);
+    const { range } = this.props;
+    return (
+      <div>
+        <input
+          id="range"
+          type="range"
+          value={range}
+          min="0"
+          max="16"
+          step="1"
+          onChange={this.updateRange}
+        />
+        <span id="output">{range}</span>
+      </div>
+    );
+  }
+}
+
+/* class Range2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rangeVal: 0,
+    };
+    this.updateRange = this.updateRange.bind(this);
+  }
+
+  updateRange(val) {
+    this.setState({
+      rangeVal: val,
+    });
+  }
+
+  render() {
+    const { rangeVal } = this.state;
+    return <Range range={rangeVal} updateRange={this.updateRange} />;
+  }
+} */
+
+export default ImportanceRange
