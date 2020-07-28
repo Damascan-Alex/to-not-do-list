0dc2bec35a7f40b064c54c5b2f47acd2a7c03e08
Damascan-Alex <doxvirtual@gmail.com>
2020-07-28 16:56:15 +0700

    scrool-functinality

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,23 +1,35 @@
 //Task list render all task including those for next day
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Task from "./task/task";
 import s from "./taskList.module.css";
 import { reduxForm, Field } from "redux-form";
+
 const TaskList = (props) => {
   const { createNewTask } = props;
   let createNewTaskX = (values) => {
-    debugger
+    debugger;
     createNewTask(values.addNewTask); // values same as Field names
   };
-
+  // When we do a double click on today list should be able to add task
   return (
     <div>
       <div className={s.taskList}>
+        <div className={s.listScrool}>
         {props.taskDivs.todayTasks.map((tsk) => (
-          <Task state ={tsk} editTask={props.editTask} task={tsk.task} done={tsk.done} importance={tsk.importance} id={tsk.id} handleDelete={props.deleteTask} />
+          <Task
+            state={tsk}
+            editTask={props.editTask}
+            task={tsk.task}
+            done={tsk.done}
+            importance={tsk.importance}
+            id={tsk.id}
+            handleDelete={props.deleteTask}
+          />
         ))}
-        <AddTaskReduxForm onSubmit={createNewTaskX} />{" "}
+        </div>
+       
+        <AddTaskReduxForm  onSubmit={createNewTaskX} />{" "}
       </div>
 
       <div className={s.tomarowLand}>
@@ -29,22 +41,26 @@ const TaskList = (props) => {
 };
 
 const AddTaskForm = ({ handleSubmit }) => {
+ 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} onBlur={handleSubmit} autoFocus={true}
+    
+     >
       <div>
         <Field
+          className={s.edit}
           component="textarea"
           name="addNewTask"
           placeholder="Add new task bro"
+          
         />
+        <button type='submit'>Add</button>
       </div>
-      <div>
-        <button>Add</button>
-      </div>
+
     </form>
   );
 };
 
-const AddTaskReduxForm = reduxForm({ form: "messageForm" })(AddTaskForm);
+const AddTaskReduxForm = reduxForm({ form: "taskForm" })(AddTaskForm);
 
 export default TaskList;
