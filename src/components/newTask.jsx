4c581bf106bf910c4c8d0e68cfc141e45e2b830f
Damascan-Alex <doxvirtual@gmail.com>
2020-07-29 08:47:41 +0700

    Refactor the code a bit

---
+++ src/components/newTask.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { reduxForm, Field } from "redux-form";
+
+const AddTaskForm = ({ handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <Field
+          //className={s.edit}
+          component="input"
+          name="addNewTask"
+          placeholder="Add new task bro"
+        />
+        <button type="submit">Add</button>
+      </div>
+    </form>
+  );
+};
+
+const AddTaskReduxForm = reduxForm({ form: "taskForm" })(AddTaskForm);
+
+export default AddTaskReduxForm;
